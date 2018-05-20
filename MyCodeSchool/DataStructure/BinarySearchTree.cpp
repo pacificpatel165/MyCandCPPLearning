3d208fa21f985266994deef60de8ba3bc65eb322
Prashant Patel <itprashantpatel@gmail.com>
2018-05-20 20:30:39 +0530

    Added more binary tree functions

--- MyCodeSchool/DataStructure/BinarySearchTree.cpp
+++ MyCodeSchool/DataStructure/BinarySearchTree.cpp
@@ -2,6 +2,7 @@
 // Simple program to create a BST of integers and search an element in it 
 #include <iostream>
 #include <new>
+#include <queue>
 
 using namespace std;
 
@@ -64,6 +65,197 @@ bool Search(BstNode* head, int data)
     }
 }
 
+int findMin(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout << "Tree is empty" << endl;
+        return -1;
+    }
+    while(root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+}
+
+int findMinRecursive(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout << "Tree is empty" << endl;
+        return -1;
+    }
+    if(root->left == NULL)
+    {
+        return root->data;
+    }
+    findMinRecursive(root->left);
+}
+
+int findMax(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout << "Tree is empty" << endl;
+        return -1;
+    }
+    while(root->right != NULL)
+    {
+        root = root->right;
+    }
+    return root->data;
+}
+
+/*
+ * Height of a Node ==> Number of edges in longest path from the node to a leaf.
+ * Height of tree ==> Height of root
+ * Height of tree with 1 node = 0
+ * Depth of a node ==> No. of edges in path from root to that node.
+ */
+
+int max(int left, int right)
+{
+    if(left >= right)
+        return left;
+    else
+       return right ;
+}
+
+int findHeight(BstNode *root)
+{
+    if(root == NULL)
+    {
+        return -1; // Since we are calaculating no. of height as edges no node.
+    }
+    return (max(findHeight(root->left), findHeight(root->right)) + 1);
+}
+
+/*
+ * Breadth-first ==> Level Order, Traverse the node on level, use queue machanism to traverse
+ */
+
+void PrintLevelOrder(BstNode *root)
+{
+    if(root == NULL)
+    {
+        cout << "Tree is empty" << endl;
+        return;
+    }
+    queue<BstNode *> discoverNode;
+    discoverNode.push(root);
+    // While there is at least one discovered node
+    while(!discoverNode.empty())
+    {
+        BstNode *visitedNode = discoverNode.front();
+        cout << visitedNode->data << " ";
+        // Push the childeren of visited node in queue
+        if(visitedNode->left != NULL)
+        {
+            discoverNode.push(visitedNode->left);
+        }
+        if(visitedNode->right != NULL)
+        {
+            discoverNode.push(visitedNode->right);
+        }
+        // Now remove the discovered node from queue
+        discoverNode.pop();
+    }
+}
+
+/*
+ * Depth-first ==> Traverse the node in 
+ * PreOrder(Data, Left, Right), InOrder(Left, Data, Right) or PostOrder(Left, Right, Data)
+ */
+
+void PrintPreOrder(BstNode *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    PrintPreOrder(root->left);
+    PrintPreOrder(root->right);
+}
+
+void PrintInOrder(BstNode *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    PrintInOrder(root->left);
+    cout << root->data << " ";
+    PrintInOrder(root->right);
+}
+
+void PrintPostOrder(BstNode *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    PrintPostOrder(root->left);
+    PrintPostOrder(root->right);
+    cout << root->data << " ";
+}
+
+// Chekc if a given binary tree is BST(Binary Search Tree)
+bool IsSubTreeLesser(BstNode* root, int value)
+{
+    if(root == NULL)
+    {
+        return true;
+    }
+    if((root->data <= value) &&
+        IsSubTreeLesser(root->left, value) &&
+        IsSubTreeLesser(root->right, value))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool IsSubTreeGreater(BstNode* root, int value)
+{
+    if(root == NULL)
+    {
+        return true;
+    }
+    if((root->data > value) &&
+        IsSubTreeGreater(root->left, value) &&
+        IsSubTreeGreater(root->right, value))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool IsBinarySearchTree(BstNode* root)
+{
+    if(root == NULL)
+    {
+        return true;
+    }
+    if( IsSubTreeLesser(root->left, root->data) &&
+        IsSubTreeGreater(root->right, root->data) &&
+        IsBinarySearchTree(root->left) &&
+        IsBinarySearchTree(root->right))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main() 
 {
@@ -78,7 +270,8 @@ int main()
     // Ask user to enter a number.
     int number;
     cout<<"Enter number be searched\n";
-    cin>>number;
+    //cin>>number;
+    number = 12;
     //If number is found, print "FOUND"
     if(Search(root,number) == true)
     {
@@ -88,5 +281,22 @@ int main()
     {
         cout<<"Not Found\n";
     }
+    cout << "Minimium number in tree " << findMin(root) << endl;
+    cout << "Maximum number in tree " << findMax(root) << endl;
+    cout << "Minimium number in tree(Recursive) " << findMinRecursive(root) << endl;
+    cout << "Height of tree " << findHeight(root) << endl;
+    cout << "Print Level Order of nodes [" ;
+    PrintLevelOrder(root);
+    cout << "]" << endl;
+    cout << "Print Pre Order of nodes [" ;
+    PrintPreOrder(root);
+    cout << "]" << endl;
+    cout << "Print In Order of nodes [" ;
+    PrintInOrder(root);
+    cout << "]" << endl;
+    cout << "Print Post Order of nodes [" ;
+    PrintPostOrder(root);
+    cout << "]" << endl;
+    cout << "Is tree BST(Binary Search Tree [" << IsBinarySearchTree(root) << "]" << endl;
     return 0;
 }
